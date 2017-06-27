e4f629a083aab7bfe3e2d390518207caf3e75703
mmsmsy <mmsmsy@gmail.com>
2017-06-27 18:04:01 +0200

    Added nextPage route to fetch new results with google places api's next_page_token. Added req.params.queries to requests

--- app.js
+++ app.js
@@ -14,22 +14,50 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/v1', router);
 
 router.get('/listplaces', (req, res) => {
-  let url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key='
-    + googleApiKey;
+  const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    + '?location=' + req.query.location
+    + '&radius=' + req.query.radius
+    + '&type=' + req.query.type
+    + '&keyword=' + req.query.keyword
+    + '&key=' + googleApiKey;
   axios.get(url)
-    .then(places => res.send(places.data.results))
-    .then(console.log('GET, /listplaces'));
+    .then(places => {
+      let placesData = {};
+      places.data.next_page_token ?
+      placesData = {results: places.data.results, nextPageToken: places.data.next_page_token} :
+      placesData = {results: places.data.results};
+      res.send(placesData);
+      console.log('GET, /listplaces');
+    });
+});
+
+router.get('/nextpage', (req, res) => {
+  const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken='
+    + req.query.nextPageToken
+    + '&key=' + googleApiKey;
+
+  axios.get(url)
+    .then(places => {
+      let placesData = {};
+      places.data.next_page_token ?
+      placesData = {results: places.data.results, nextPageToken: places.data.next_page_token} :
+      placesData = {results: places.data.results};
+      res.send(placesData);
+      console.log('GET, /nextplaces');
+    });
 });
 
 router.get('/getphoto/:reference', (req, res) => {
-  let url = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='
+  const url = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='
     + req.params.reference
     + '&key='
     + googleApiKey;
 
   axios.get(url)
-    .then(photo => res.send(photo.request.res.responseUrl))
-    .then(photo => console.log(`GET, /getphoto/${req.params.reference}`));
+    .then(photo => {
+      res.send(photo.request.res.responseUrl)
+      console.log('GET, /getphoto')
+    });
 });
 
 app.listen(3001, () => console.log("Server running at 3001"));
